73efbedd9962c357bbf9d5469cd657e04fb6e371
@NavjotRana <navjot@everwell.org>
2020-05-16 21:58:07 +0530

    project sync

--- app/src/main/java/com/bezatnew/bezat/fragments/SearchRetailer.java
+++ app/src/main/java/com/bezatnew/bezat/fragments/SearchRetailer.java
@@ -3,6 +3,7 @@ package com.bezatnew.bezat.fragments;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
