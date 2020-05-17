cea98d97447d7c7c1cc067c8e22f92652f8a8de8
Aditya <kathpalia.aditya11051999@gmail.com>
2020-05-17 15:28:19 +0530

    updated change language spinner

--- app/src/main/java/com/bezatnew/bezat/fragments/ChangeLanguage.java
+++ app/src/main/java/com/bezatnew/bezat/fragments/ChangeLanguage.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -13,6 +14,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +26,8 @@ import com.bezatnew.bezat.R;
 import com.bezatnew.bezat.activities.Homepage;
 import com.bezatnew.bezat.utils.SharedPrefs;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -96,9 +102,68 @@ public class ChangeLanguage extends Fragment implements View.OnClickListener {
         txtEnglish.setOnClickListener(this);
         txtArabic.setOnClickListener(this);
 
+        List<String> l = new ArrayList<>();
+        l.add("");
+        l.add("English");
+        l.add("Arabic");
+
         Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner_language);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
-                R.array.array_languages, R.layout.spinner_item_language);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(),
+                R.layout.spinner_item_language,
+                R.id.tv_language,
+                l){
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                LayoutInflater inflater = ChangeLanguage.this.getLayoutInflater();
+
+                if(convertView == null){
+                    convertView = inflater.inflate(R.layout.spinner_item_language,parent, false);
+                }
+                TextView txtTitle = (TextView) convertView.findViewById(R.id.tv_language);
+                txtTitle.setText(l.get(position));
+                ImageView imageView = (ImageView) convertView.findViewById(R.id.iv_language);
+                if(position==1){
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.english_icon));
+                }else if(position==2){
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.arabic_icon));
+                }
+                else{
+                    imageView.setImageDrawable(null);
+                }
+
+                return convertView;
+            }
+        };
+        /*{
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        }*/;
         adapter.setDropDownViewResource(R.layout.spinner_item_language);
         spinner.setAdapter(adapter);
         spinner.setPrompt("Change language");
@@ -111,8 +176,12 @@ public class ChangeLanguage extends Fragment implements View.OnClickListener {
 
                 }else if(position==1){
                     setLocale("en");
+                    ImageView v = view.findViewById(R.id.iv_language);
+                    v.setImageDrawable(getResources().getDrawable(R.drawable.english_icon));
                 }else{
                     setLocale("ar");
+                    ImageView v = view.findViewById(R.id.iv_language);
+                    v.setImageDrawable(getResources().getDrawable(R.drawable.arabic_icon));
                 }
             }
 
